de7712349495bb1b36349c880eddf5e7049e7ca7
Daudov, Tymur <tymur.daudov@f-secure.com>
2021-03-31 02:08:48 +0300

    Rename to takenouchi-ryu

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/takeno-uchi-ryu'),
+      dir: require('path').join(__dirname, './coverage/takenouchi-ryu'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'takeno-uchi-ryu'`, () => {
+  it(`should have as title 'takenouchi-ryu'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('takeno-uchi-ryu');
+    expect(app.title).toEqual('takenouchi-ryu');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('takeno-uchi-ryu app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('takenouchi-ryu app is running!');
   });
 });
